997b7202b98b4188fecf8abb2bae104b35569e1f
Evan-2007 <65788884+Evan-2007@users.noreply.github.com>
2024-09-25 00:42:07 -0700

    Refactor CSS variables and layout in globals.css, layout.tsx, and main.tsx

--- src/app/home/layout.tsx
+++ src/app/home/layout.tsx
@@ -7,9 +7,13 @@ export default function HomeLayout({
     children: React.ReactNode
   }) {
     return(
-      <div className='w-full h-screen flex flex-col'>
+      <div className='w-full h-screen flex flex-col bg-card'>
         <header className='h-10 w-screen absolute z-50'></header>
-        {children}
+        <div className='w-full h-full pr-4 pl-4 pt-4 '>
+        <div className='w-full h-full border-border border rounded-xl bg-background '>
+          {children}
+        </div>
+        </div>
         <Player />
       </div>
     )
--- src/components/player/main.tsx
+++ src/components/player/main.tsx
@@ -114,7 +114,7 @@ export function PlayerContent() {
 
     if (songData == null || credentials.username == null || credentials.password == null || credentials.salt == null) {
         return (
-            <div className="h-[100px] w-screen bg-card border-border border-t sticky flex">
+            <div className="h-[100px] w-screen  sticky flex">
                 <div className='flex h-full justify-center items-center'>
                     <div className='h-full p-3'>
                         <div  className='h-full rounded-lg aspect-square bg-background'>
@@ -132,7 +132,7 @@ export function PlayerContent() {
     }
 
     return (
-        <div className="h-[100px] w-screen bg-card border-border border-t sticky flex justify-between">
+        <div className="h-[100px] w-screen  sticky flex justify-between">
             <div className='flex h-full justify-center items-center absolute'>
                 <div className='h-full group'>
                     <div  className='h-full rounded-lg aspect-square absolute p-3 z-50'>
@@ -259,49 +259,49 @@ const Controls: React.FC<{ songData: Song, audioRef : any }> = ({ songData, audi
                     {
                         shuffle ? 
                         <button onClick={() => setShuffle(false)}>
-                            <Shuffle color='red' strokeWidth={3} absoluteStrokeWidth/>
+                            <Shuffle color='red' strokeWidth={3} absoluteStrokeWidth size={20} className="mr-2"/>
                         </button> 
                         :
                         <button onClick={() => setShuffle(true)}>
-                            <Shuffle  strokeWidth={3} absoluteStrokeWidth/>
+                            <Shuffle  strokeWidth={2.5} absoluteStrokeWidth size={20} className="mr-2"/>
                         </button>
                     }
                     <button>
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-[30px]">
                             <path d="M9.195 18.44c1.25.714 2.805-.189 2.805-1.629v-2.34l6.945 3.968c1.25.715 2.805-.188 2.805-1.628V8.69c0-1.44-1.555-2.343-2.805-1.628L12 11.029v-2.34c0-1.44-1.555-2.343-2.805-1.628l-7.108 4.061c-1.26.72-1.26 2.536 0 3.256l7.108 4.061Z" />
                         </svg>
                     </button>
                     {
                         playing ? 
                         <button onClick={() => setPlaying(false)}>
-                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-[30px]">
                                 <path fillRule="evenodd" d="M6.75 5.25a.75.75 0 0 1 .75-.75H9a.75.75 0 0 1 .75.75v13.5a.75.75 0 0 1-.75.75H7.5a.75.75 0 0 1-.75-.75V5.25Zm7.5 0A.75.75 0 0 1 15 4.5h1.5a.75.75 0 0 1 .75.75v13.5a.75.75 0 0 1-.75.75H15a.75.75 0 0 1-.75-.75V5.25Z" clipRule="evenodd" />
                             </svg>
                         </button> 
                         :
                         <button onClick={() => setPlaying(true)} >
-                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-[30px]">
                                 <path fillRule="evenodd" d="M4.5 5.653c0-1.427 1.529-2.33 2.779-1.643l11.54 6.347c1.295.712 1.295 2.573 0 3.286L7.28 19.99c-1.25.687-2.779-.217-2.779-1.643V5.653Z" clipRule="evenodd" />
                             </svg>
                         </button>
                     }
                     <button>
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-[30px]">
                             <path d="M5.055 7.06C3.805 6.347 2.25 7.25 2.25 8.69v8.122c0 1.44 1.555 2.343 2.805 1.628L12 14.471v2.34c0 1.44 1.555 2.343 2.805 1.628l7.108-4.061c1.26-.72 1.26-2.536 0-3.256l-7.108-4.061C13.555 6.346 12 7.249 12 8.689v2.34L5.055 7.061Z" />
                         </svg>
                     </button>
                     {
                         repeat === 2 ?
                         <button onClick={() => setRepeat(0)}>
-                            <Repeat1 color='red' strokeWidth={3} absoluteStrokeWidth/>
+                            <Repeat1 color='red' strokeWidth={2.5} absoluteStrokeWidth size={24} className="ml-2"/>
                         </button> 
                         : repeat === 1 ?
                         <button onClick={() => setRepeat(2)}>
-                            <Repeat strokeWidth={3} absoluteStrokeWidth color='red'/>
+                            <Repeat strokeWidth={2.5} absoluteStrokeWidth color='red' size={24} className="ml-2"/>
                         </button>
                         :
                         <button onClick={() => setRepeat(1)}>
-                            <Repeat strokeWidth={3} absoluteStrokeWidth/>
+                            <Repeat strokeWidth={2.5} absoluteStrokeWidth size={24} className="ml-2"/>
                         </button>
                     }
                 </div>
