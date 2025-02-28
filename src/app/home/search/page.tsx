cbd752af435442182c7c0a3b67df3f2c9f230b0a
Evan-2007 <65788884+Evan-2007@users.noreply.github.com>
2025-02-28 00:06:38 -0800

    Add full page for searching with filtering and created song item component

--- src/app/home/search/page.tsx
+++ src/app/home/search/page.tsx
@@ -3,11 +3,13 @@ import { SourceManager } from '@/lib/sources/source-manager';
 import { useSearchParams } from 'next/navigation';
 import { Separator } from '@/components/ui/separator';
 import { Button } from '@/components/ui/button';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { Suspense } from 'react';
+import { searchResult } from '@/lib/sources/types';
+import {SongItem} from '@/components/song/song-item';
+
 
 export default function Search() {
-  const sourceManager = SourceManager.getInstance();
   const [tab, setTab] = useState('Top Results');
 
   return (
@@ -53,7 +55,7 @@ export default function Search() {
         </div>
         <Separator />
       </div>
-      <div>
+      <div className='flex w-full flex-col items-center h-full'>
         <Suspense fallback={<div>Loading...</div>}>
           <Results tab={tab} />
         </Suspense>
@@ -64,7 +66,55 @@ export default function Search() {
 
 function Results({ tab }: { tab: string }) {
   const searchParams = useSearchParams();
-  const search = searchParams.get('query');
+  const sourceManager = SourceManager.getInstance();
 
-  return null;
+  const search = searchParams.get('query');
+  const [loading, setLoading] = useState(true);
+  const [results, setResults] = useState<null | searchResult>(null);
+
+  const getResults = async () => {
+    setLoading(true);
+    const result = await sourceManager.search(search ?? '');
+    setResults(result);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    getResults()
+  }, [search])
+  
+
+  
+
+  return (
+    <div className='flex w-full flex-col items-center'>
+      {loading ? (
+        <div>Loading...</div>
+      ) : (
+        <div className='flex w-full flex-col items-center'>
+          {results ? (
+            <div className='flex w-full flex-col items-center'>
+              {results.songs.length > 0 && results.songs.map((song, index) => (
+                <div key={index} className={`w-full flex flex-col items-center ${tab === 'Tracks' ? 'block' : 'hidden'}`}>
+                  <SongItem data={song} type='song'/>
+                </div>
+              ))}
+              {results.albums.length > 0 && results.albums.map((album, index) => (
+                <div key={index} className={`w-full flex flex-col items-center ${tab === 'Albums' ? 'block' : 'hidden'}`}>
+                  <SongItem data={album} type='album'/>
+                </div>
+              ))}
+              {results.artists.length > 0 && results.artists.map((artist, index) => (
+                <div key={index} className={`w-full flex flex-col items-center ${tab === 'Artists' ? 'block' : 'hidden'}`}>
+                  <SongItem data={artist} type='artist'/>
+                </div>
+              ))}
+        </div>
+          ) : (
+            <div>No results found.</div>
+          )}
+        </div>
+      )}
+    </div>
+  );
 }
--- src/components/song/song-item.tsx
+++ src/components/song/song-item.tsx
@@ -1,3 +1,57 @@
-export function SongItem() {
-  return null;
+import { song as Song, albums as Album, artists as Artist } from '@/lib/sources/types';
+import {SourceManager} from '@/lib/sources/source-manager';
+import { useRouter } from 'next/navigation';
+import { useQueueStore } from '@/lib/queue';
+
+type SongItemProps =
+  | { type: 'song'; data: Song }
+  | { type: 'playlist'; data: any }
+  | { type: 'album'; data: Album }
+  | { type: 'artist'; data: Artist }
+  | { type: 'video'; data: any };
+
+export function SongItem({data, type}: SongItemProps) {
+  
+  const router = useRouter();
+  const play = useQueueStore((state) => state.play);
+  const handleClick = () => {
+
+    if (type === 'song') {
+      play(data);
+    } else if (type === 'playlist') {
+      router.push(`/playlist/${data.source}/${data.id}`);
+    } else if (type === 'album') {
+      router.push(`/album/${data.source}/${data.id}`);
+    }
+    else if (type === 'artist') {
+      router.push(`/artist/${data.source}/${data.id}`);
+    }
+    else if (type === 'video') {
+      router.push(`/video/${data.source}/${data.id}`);
+    }
+
+  };
+
+  return (
+    <div className='flex flex-row items-start space-x-3 p-2 hover:bg-gray-800 w-11/12' onClick={() => handleClick()}>
+      <img src={data.imageUrl} alt={data.title} className={`w-12 h-12 ${type === 'song' ? 'rounded-md' : type === 'artist' ? 'rounded-lg' : ''}`} />
+      <div className='flex flex-col w-full'>
+        {type === 'song' ? (
+          <>
+            <p className='text-white font-bold'>{data.title}</p>
+            <p className='text-gray-400'>{data.artist}</p>
+          </>
+        ) : type === 'artist' ? (
+          <p className='text-white font-bold'>{data.name}</p>
+        ) : type === 'album' ? (
+          <p className='text-white font-bold'>{data.title}</p>
+        ) : type === 'playlist' ? (
+          <p className='text-white font-bold'>{data.title}</p>
+        ) : (
+          <p className='text-white font-bold'>{data.title}</p>
+        )}
+                
+      </div>
+    </div>
+  );
 }
