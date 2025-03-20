7382f55143cc09f22154d6bea31c2fd4f3a70583
Evan-2007 <65788884+Evan-2007@users.noreply.github.com>
2025-03-20 13:53:18 -0700

    Add HTTP plugin to bypass cors for music.apple.com

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,49 +1,49 @@
-
+use serde_json::json;
+use tauri::{AppHandle, Emitter};
 use tauri_plugin_deep_link::DeepLinkExt;
 use url::Url;
-use tauri::{AppHandle, Emitter};
-use serde_json::json;
 
 #[cfg(not(target_os = "ios"))]
 use tauri_plugin_single_instance::init as single_instance_init;
 
-
 fn handle_deep_link(app: AppHandle, deep_link: &str) {
     let url = Url::parse(deep_link).unwrap();
     match url.domain() {
-        Some("oauth2") => {
-            match url.path() {
-                "/callback/tidal" => {
-                    if let Some((_, code)) = url.query_pairs().find(|(key, _)| key == "code") {
-                        println!("OAuth2 code: {}", code);
-                        let _ = app.emit("tidal-auth-response", json!({
+        Some("oauth2") => match url.path() {
+            "/callback/tidal" => {
+                if let Some((_, code)) = url.query_pairs().find(|(key, _)| key == "code") {
+                    println!("OAuth2 code: {}", code);
+                    let _ = app.emit(
+                        "tidal-auth-response",
+                        json!({
                             "code": code
-                        }));
-                    } else {
-                        println!("No code found in OAuth2 callback");
-                    }
-                },
-                _ => {
-                    println!("Unknown OAuth2 deep link: {}", deep_link);
+                        }),
+                    );
+                } else {
+                    println!("No code found in OAuth2 callback");
                 }
+            }
+            _ => {
+                println!("Unknown OAuth2 deep link: {}", deep_link);
             }
         },
         _ => {
             println!("Unknown deep link: {}", deep_link);
         }
     }
-
 }
-
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
-    let builder = tauri::Builder::default();
+    let builder = tauri::Builder::default().plugin(tauri_plugin_http::init());
 
     // Only add the single instance plugin for non-iOS targets.
     #[cfg(not(target_os = "ios"))]
     let builder = builder.plugin(single_instance_init(|_app, argv, _cwd| {
-        println!("New instance opened with deep link arguments: {:?}", &argv[1]);
+        println!(
+            "New instance opened with deep link arguments: {:?}",
+            &argv[1]
+        );
         handle_deep_link(_app.clone(), &argv[1]);
     }));
 
