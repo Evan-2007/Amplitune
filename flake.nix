a97cb4b55aa36d699256bc7782d13136ffa3974b
Evan Cooper <65788884+Evan-2007@users.noreply.github.com>
2025-04-13 22:31:07 -0700

    fix: update development environment configuration in flake.nix to include necessary nativeBuildInputs and reorder buildInputs

--- flake.nix
+++ flake.nix
@@ -5,6 +5,7 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
+  
 
   outputs = { self, nixpkgs, ... }@inputs:
     let
@@ -16,30 +17,39 @@
 
     in
     {
+
       devShells = forAllSystems (system:
         let
           pkgs = pkgsFor system;
         in
         {
           default = pkgs.mkShell {
-            buildInputs = with pkgs; [
-              cargo
-              git
-              nodejs-18_x
-              nodePackages.pnpm
+              nativeBuildInputs = with pkgs; [
               pkg-config
-              glib
-              atk
+              gobject-introspection
+              cargo
+              cargo-tauri
+              nodejs
+            ];
+            buildInputs = with pkgs; [
+              at-spi2-atk
+              atkmm
               cairo
-              pango
+              gdk-pixbuf
+              glib
+              gtk3
+              harfbuzz
+              librsvg
               libsoup_3
+              pango
               webkitgtk_4_1
-              haskellPackages.gi-javascriptcore
+              openssl
             ];
 
             shellHook = ''
               echo "Entered Rust/Node.js development environment."
               echo "Available tools: Rust/Cargo, Node $(node --version), PNPM $(pnpm --version), Git $(git --version)"
+              export WEBKIT_DISABLE_COMPOSITING_MODE=1
             '';
           };
         });
